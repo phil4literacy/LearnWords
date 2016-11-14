511bcd5811f15bee6deab80a32279943f7b15a2a
hhzl <hannes.hirzel@gmail.com>
2016-11-14 21:16:47 +0100

    added LW.wdsDB.removeWords()

--- js/all.js
+++ js/all.js
@@ -105,6 +105,42 @@ if(typeof(LW.wdsDB) == 'undefined' || LW.wdsDB == null || !LW.wdsDB){
 				}
 			}
 		},
+
+		removeWords: function(){
+			if (LW.wdsDB.isOK) {
+                        "use strict";
+                        var key;
+                        var st; 
+                        var keysToDelete = [];
+
+                        var prefixForNumber = 'learnWords-index';  
+
+                        // go through all keys starting with the name
+                        // of the database, i.e 'learnWords-index14'
+                        for (var i = 0; i < localStorage.length; i++){
+                            key = localStorage.key(i);
+                            st = localStorage.getItem(key);                            
+    
+                            if (key.lastIndexOf(prefixForNumber,0) === 0) {
+                                keysToDelete.push(key);
+                            };
+			};
+                        // now we have all the keys which should be deleted
+                        // in the array keysToDelete.
+                        console.log(keysToDelete);
+                        keysToDelete.forEach(function(aKey){
+                             localStorage.removeItem(aKey);
+			});
+
+                        // reset index
+                        LW.wdsDB.storeItem('learnWords-words', '');
+
+                        // maybe this as well
+                        localStorage.removeItem('learnWords-settings');
+                        }
+
+		},
+
 		
 		init: function(){
 			LW.wdsDB.isOK = false;
